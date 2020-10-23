8938746640c67606e50faae5220754c2e99388d3
Alvif Sandana Mahardika <55418227+AlvifSandana@users.noreply.github.com>
2020-10-23 07:48:06 +0700

    Create tes.js

---
+++ tes.js
@@ -0,0 +1,24 @@
+// compile class dulu dari ts
+var tes = /** @class */ (function () {
+    function tes(txt) {
+        if (txt === void 0) { txt = "hello"; }
+        this.teks = txt;
+    }
+    tes.prototype.show = function () {
+        return this.teks;
+    };
+    return tes;
+}());
+
+
+// fungsi readline
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('Who are you?', name => {
+    var ttt = new tes(name);
+    console.log(`Hey there ${ttt.show()}!`);
+    readline.close();
+});
